514a79faaf205593fdbcadcc5c9b27fb50f83eaa
Muhammad Faizan Tariq <115633021+Faizan-26@users.noreply.github.com>
2023-09-26 23:23:51 +0500

    QueueArrayADt

---
+++ Assignments/DailyTasks/Project1/Project1/QueueArrayADT.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+class QueueArrayAdt {
+	int size;
+	int rear;
+	int front;
+	int numItems;
+	int* array;
+public:
+	QueueArrayAdt(int size) {
+		this->size = size;
+		front= -1;
+		rear = -1;
+		numItems = 0;
+		array = new int[size];
+	}
+	bool isEmpty() {
+		return numItems == 0;
+	}
+	bool isFull() {
+		return numItems == size - 1;
+	}
+	void enqueue(int data) {
+		if (isFull()) {
+			cout << "\n Queue is Full.\n";
+			return;
+		}
+		rear = (rear + 1) % size;
+		array[rear] = data;
+		numItems++;
+		cout << "\nData is added in array Adt.\n";
+	}
+	void dequeue() {
+		if (isEmpty()) {
+			cout << "\nQueue is Empty.\n";
+			return;
+		}
+		front = (front + 1) % size;
+		cout << array[front] << " removed from queue.\n";
+		numItems--;
+	}
+	~QueueArrayAdt() {
+		front = -1;
+		rear = -1;
+		numItems = 0;
+		delete[] array;
+	}
+};
+int main() {
+	QueueArrayAdt a(4);
+	a.enqueue(1);
+	a.enqueue(2);
+	a.enqueue(3);
+	a.enqueue(4);
+	a.dequeue();
+	a.dequeue();
+	a.dequeue();
+	return 0;
+}
